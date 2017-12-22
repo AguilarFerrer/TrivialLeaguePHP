0daf32c8a5fac607ed18b643e32064aec650e2ac
Sergix8 <34548522+Sergix8@users.noreply.github.com>
2017-12-22 02:09:57 +0100

    Add files via upload
    No se porque no me van, he estado mirando manual y lo veo igual a como los he hecho

---
+++ SQL/Inserts.sql
@@ -0,0 +1,231 @@
+INSERT INTO Trivial.Topic(Name) VALUES (Sports)
+INSERT INTO Trivial.Topic(Name) VALUES(Culture)
+INSERT INTO Trivial.Topic(Name) VALUES(Cinema)
+INSERT INTO Trivial.Topic(Name) VALUES(Geography)
+INSERT INTO Trivial.Topic(Name) VALUES(Art)
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES("The lakers removed from the franchise 2 shirts of Kobe Bryant, which ones?",
+	"8 &24","32 &8","24 & 44","42,32")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","How many championships has Fernando Alonso(2016)?","2","1","3","0")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who won the last World Cup of football(2016)?","Germany","Spain","Argentina","Brasil")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","How many players are in the field of basketball, between both teams?","10","11","5","22")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who is called 'The Doctor' at motorbikes?","Valentino Rossi","Marc Marquez","Jorge Lorenzo","Maverick Viñales")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Which player scored 100 points in a game of basketball?","Wilt Chamberlain","Kobe Bryant","Hakeem Olajuwon","Michael Jordan")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who has the record of triple-double in a season of NBA?","Rusell Westbrook","Oscar Robertson","Lebron James","Michael Jordan")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who has the record of championships in F1(2016)?","Michael Schumacher","Alain Prost","Ayrton Senna")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who won last Champions League edition (2016)?","Real Madrid","Barcelona","Juventus","Bayern Munich")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Which player is known as 'El niño maravilla'?","Alexis Sanchez","Lionel Messi","Cristiano Ronaldo","Angel Di Maria")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","In how many teams have played Francesco Totti?","1","2","3","4")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","In what position was selected Carmelo Anthony at Draft?","3","1","2","4")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Which explayer started his career at the benches in NBA?","Jason Kidd","Shaquille O`Neal","Larry Bird","Michael Jordan")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","What did Michael Phelps during is career?","Swim","Run","Basketball","Football"
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Sports","Who is Usain Bolt?","A sprinter","A swimmer","A football player","A basketball player"
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Which river passes through 'Cataluña'?","Ebro","Tajo","Guadalquivir","Turia")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Where is located the national park of 'Doñana'?","Huelva","Madrid","Valladolid","Huesca")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Which city is called the capital of love?","Paris","Roma","Venice","London")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Which city is characteristic for their gondolas?","Venice","Manchester","Munich","Barcelona")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Where is located the Big Ben?","London","Sheffield","Birmingham","Leicester")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Which is the capital of Spain?","Madrid","Barcelona","Bilbao","Sevilla")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","In which country are the ruins of Sparta?","Greece","Italy","Spain","Morocco")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Which is the longest river in the world?","Nile","Amazonas","Ebro","Yangtse")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","How many continents are in the world?","6","5","11","10")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","What is the largest island in the Mediterranean Sea?","Sicily","Majorca","Cyprus","Sardinia")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","What is the bigger mountain in the world?","Everest","Teide","Kilimanjaro","K2")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","What is the most populated country in the world?","China","India","Japan","Rusia")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","Where is located Grand Canyon?","EEUU","Rusia","Mexico","Canada")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","What is the smallest country in the world?","El Vaticano","Monaco","San Marino","Finland"
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Geography","What is the capital of Canada?","Ottawa","Toronto","Texas","Vancouver")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Where the first olympic games were created?","Olimpia","Rome","Turin","Venice")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Who wrote 'El Quijote'?","Miguel de Cervantes","Pablo Picasso","Pablo Neruda","Arturo Pérez-Reverte")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","In which year came Cristobal Colón to America?","1492","1936","1514","1874")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","What is the most produced product in Guatemala?","Cafe","Rice","Fruits","Milk")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Who is known as The King Of Rock?","Elvis Presley","Led Zeppelin","David Bowie","Elton Jhon")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","What studies cartography?","Maps","Rivers","Seas","Coasts")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Which coin use in United Kingdom?","Pound","Euro","Dolar","Penny")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Which is the color of hope?","Green","Purple","Yellow","Red")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","How many legs has a spider?","8","6","7","10")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Where is the monument 'Taj Mahal'","India","Morocco","France","Egypt")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Which name had the general of the 'Nazis' during the WWII?","Adolf Hitler","Eric Bill","Sebastian Bëhr","Roman Reus")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Which year did the First World War Started?","1914","1916","1936","192")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","Which name has the creator of Facebook?","Mark Zuckerberg","Mark Brown","Stephen Simmons","Walden Smith")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","The year that the Spanish Constitution was aproved...","1978","1980","1938","1940")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Culture","The first atomic bomb impacted in...","Japan","China","India","Korea")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who is the protagonist of the film 'Rocky'?","Sylverter Stallone","Memo Ochoa","Jason Staham","Adam Sandler")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Which of the following films the actor 'Jhonny Deep' doesn't appear","The Equalizer","Pirates of The Caribbean","The mask","Alice in Wonderland")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Which actor is also called 'The Rock', for is past in WWE?","Dwayne Jhonson","Jhon Cena","Kevin Hart","Jack Black")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","How many films have Star Wars?(2017)","9","6","7","8")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Which name has the evil neighbour in the film 'Toy Story'?","Sid","Tommy","Pedro","Jhon")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Which color is the suit of 'The Incredibles'?","Red","Green","White","Blue")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who filmed 'The Adventures of Tintin: The Secret of the Unicorn'?","Steven Spielberg","Mark Douglas","Pedro Almodovar","Charlie Sheen")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who is the character 'Logan' of X-Men","Hugh Jackman","Ben Afleck","Adam Sandler","Jhonny Deep")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who produced Monsters Inc.?","Pixar","21th Century Fox","Axn","Syfy")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who bought 21th Century Fox in 2017?","Disney","Pixar","Disapeared","Netflix")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who interprets Robert Downey Jr. in Avengers","Iron Man","Captain America","Nick Fury","Anyone")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","At the film 'The hunger games', it's divided by Districts, how many?","13","11","10","12")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Of how many movies is The Godfather filmed?","3","2","1","5")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who did the films of 'Harry Potter'?","Warner Bros & David Heyman","J.K. Rowling","21th Century Fox","LucasFilm")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Cinema","Who interprets 'Ali G'?","Baron Cohen","Adam Sandler","Jhonny Deep","Mark Mylad")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","In what century was born Velazquez?","XVII","XV","XVIII","XVI")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","Where we can find the 'Mona Lisa'?","Louvre","Museum of Prado","British Museum","Galeria Uffizi")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","In what century did rebirth arise?","XV","XIV","XVI","XVII")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","What year was Goya born?","1746","1806","1706","1796")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","In what current Da Vinci was inspired?","High Renaissance","Baroque","Realism","Mannerism")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","From where is 'Joan Miro'?","Majorca","Barcelona","Valencia","Madrid")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","In which century started Renaissance?","XV","XIV","XVII","XVIII")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","What group founded Jimmy Page?","Led Zeppelin","ACDC","Black Sabath","Iron Maiden")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","The beatles style was...","Pop/Rock","Hard Metal","Rock","Pop")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","Which singer was known as 'The Big O'?","Roy Orbison","Ozzy Osbourne","Des o'Connor","Jhon Lennon")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","How many components had The Beatles?","4","3","2","1")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","Roxanne is a song of...","The police","The Beatles","Elton Jhon","Elvis Presley")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","Where was born Noel Gallagher?","Manchester","London","Liverpool","Leicester")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","From where were 'The Rolling Stones'?","London","Liverpool","Manchester","Sheffield")
+
+INSERT INTO Trivial.Questions(Topic,Question,AnsCorrect,AnsA,AnsB,AnsC)
+VALUES ("Art","Which genre hasn't done Mick Jagger?","Metal","Rock","Pop","Blues")
