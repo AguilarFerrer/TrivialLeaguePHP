268bbd23f104d0f62e12b51bd4ebd3128a5c70c7
AguilarFerrer <felixaguilarferrer@gmail.com>
2018-01-09 19:39:24 +0100

    Added redirect for swap pages.
    This is for moving between pages.

---
+++ includes/redirect.php
@@ -0,0 +1,9 @@
+<?php
+function redirect ($page = 'index.php') {
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	$url = rtrim($url, '/\\');
+	$url .= '/' . $page;
+	header("Location: $url");
+	exit();
+}
+?>
