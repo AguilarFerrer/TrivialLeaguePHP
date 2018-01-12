474b3ec91b171c3164d3a71e3a6125360c59d4dd
AguilarFerrer <felixaguilarferrer@gmail.com>
2018-01-12 13:29:40 +0100

    Improving and fixing the form
    -Added styles to the form.
    -Images for topics added.
    -Fixed an error where the user can not submit the form.

--- Game.php
+++ Game.php
@@ -4,47 +4,49 @@
 
     $txt = '';
 
-    if($_SERVER['REQUEST_METHOD']=='POST'){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
         
         
     }
         //if error de seleccion preguntas
         $last = 0;
         $log = false;
-        $random1 = rand(1, 15);
-        $random2 = rand(1, 5);
+        
         while($log == false) {
+            $random1 = rand(1, 15);
+            $random2 = rand(1, 5);
             $q = "SELECT QuestionID, Question, AnsCorrect, AnsA, AnsB, AnsC  FROM Questions WHERE TopicQuestion=$random1 AND Topic=$random2";
             $r = @mysqli_query ($dbc, $q);
             if (mysqli_num_rows($r) == 1) {
-                //mirar si es = a la ultima pregunta.
                 $question = mysqli_fetch_array ($r, MYSQLI_ASSOC); 
-                //guardar el valor de question ultima ID
-                $q = "SELECT Name, Image  FROM Topics WHERE TopicID=$random2";
-                $r = @mysqli_query ($dbc, $q);
-                if (mysqli_num_rows($r) == 1) { 
-                    $topic = mysqli_fetch_array ($r, MYSQLI_ASSOC);
-                    mysqli_close($dbc);
-                    $log = true; 
+                if($last != $question['QuestionID']){
+                    $last = $question['QuestionID'];
+                    $q = "SELECT Name, Image  FROM Topics WHERE TopicID=$random2";
+                    $r = @mysqli_query ($dbc, $q);
+                    if (mysqli_num_rows($r) == 1) { 
+                        $topic = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+                        mysqli_close($dbc);
+                        $log = true; 
+                    }
                 }
-            }
-            else{
-                $random1 = rand(1, 15);
-                $random2 = rand(1, 5);
             }
         }
         
-        $txt = $topic['Name'] . $question['Question'] . '<br>
-            <form action="Game.php" method="post">
-            <input type="radio" name="A" value="' . $question['AnsCorrect'] . '">' . $question['AnsCorrect'] . '<br>
-            <input type="radio" name="B" value="' . $question['AnsA'] . '">' . $question['AnsA'] . '<br>
-            <input type="radio" name="C" value="' . $question['AnsB'] . '">' . $question['AnsB'] . '<br>
-            <input type="radio" name="D" value="' . $question['AnsC'] . '">' . $question['AnsC'] . '<br>
-            <input type="submit" values="submit"
-            </form>';
-    
+        $txt = '<div id="content"><h1><img src=' . $topic['Image'] . 'height="50" width="50">' . $topic['Name'] . '</h1><br><br>' . '
+            <h2>' . $question['Question'] . '</h2>
+            <p><form action="Game.php" method="POST">
+            <input type="radio" name="ans" value="' . $question['AnsCorrect'] . '">' . $question['AnsCorrect'] . '<br><br>
+            <input type="radio" name="ans" value="' . $question['AnsA'] . '">' . $question['AnsA'] . '<br><br>
+            <input type="radio" name="ans" value="' . $question['AnsB'] . '">' . $question['AnsB'] . '<br><br>
+            <input type="radio" name="ans" value="' . $question['AnsC'] . '">' . $question['AnsC'] . '<br><br>
+            <input type="hidden" name="last" value="' . $last . '">
+            <input type="submit" values="submit">
+            </form></p></div>';
         echo $txt;
 
     include('./includes/footer.html');
 ?>
 
+<form>
+    <input type="radio" onselect="submit">
+</form>
