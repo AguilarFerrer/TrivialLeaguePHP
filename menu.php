ddb197bc1af1230695bb1e400c9f92d298f39f21
Sergix8 <34548522+Sergix8@users.noreply.github.com>
2018-01-18 15:07:52 +0100

    Add files via upload
    DO the error if there are more than 5 games and a cookie for it.

--- menu.php
+++ menu.php
@@ -2,8 +2,34 @@
     include ('./includes/headerlogin.html');
     require ('./SQL/mysqli_connect.php');
 
+    if(isset($_COOKIE['GAMES'])){
+
+        if (count($_COOKIE['GAMES'])<=5){
+            // array_push($_COOKIE['GAMES'], );
+        }else{
+            redirect('menu.php?error=1');
+        }
+
+    }else{
+        // setcookie('GAMES', ['']);
+    }
+
+    if(isset($_REQUEST['error'])){
+        $error = intval($_REQUEST['error']);
+        // var_dump($error);exit();
+    }
+
 ?>
     <div id="content">
+        <?php 
+            if(isset($error)){
+                if($error == 1){
+                    ?> 
+                    <p>ERROR YA HAS PASADO LOS 5 JUEGOS</p>
+                    <?php
+                }
+            }
+        ?>
         <h1>Are you ready <?php if(isset($_COOKIE['Name'])){ echo $_COOKIE['Name'];} ?> ?</h1><br><br>
         <p> <?php
                 if(isset($_COOKIE['ID'])){
