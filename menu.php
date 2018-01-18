49293e6ab07f3bbd348a2bcb2099ed70729bbdb1
Sergix8 <34548522+Sergix8@users.noreply.github.com>
2018-01-18 20:43:27 +0100

    Add files via upload
    Less than 5 games open, and error if its not true

--- menu.php
+++ menu.php
@@ -1,48 +1,42 @@
 <?php
-    include ('./includes/headerlogin.html');
+    include ('./includes/headermenu.html');
     require ('./SQL/mysqli_connect.php');
 
-    if(isset($_COOKIE['GAMES'])){
 
-        if (count($_COOKIE['GAMES'])<=5){
-            // array_push($_COOKIE['GAMES'], );
-        }else{
+
+        if (isset($_GET['GAMES'])){
+            ?><p>Your game ID is</p> <?php $_GET['GAMES']
+
+            }else{
             redirect('menu.php?error=1');
         }
 
     }else{
-        // setcookie('GAMES', ['']);
+      
     }
 
-    if(isset($_REQUEST['error'])){
+    if(isset($_GET['error'])){
         $error = intval($_REQUEST['error']);
-        // var_dump($error);exit();
+       
     }
 
 ?>
     <div id="content">
-        <?php 
-            if(isset($error)){
-                if($error == 1){
-                    ?> 
-                    <p>ERROR YA HAS PASADO LOS 5 JUEGOS</p>
-                    <?php
-                }
-            }
-        ?>
         <h1>Are you ready <?php if(isset($_COOKIE['Name'])){ echo $_COOKIE['Name'];} ?> ?</h1><br><br>
         <p> <?php
                 if(isset($_COOKIE['ID'])){
                     $id = $_COOKIE['ID'];
                     $q = "SELECT GameID FROM Games WHERE UserID='$id'";
                     $r = @mysqli_query ($dbc, $q);
-                    //working.....
                     $row= mysqli_fetch_row($r);
                     while(!empty($row)) {    
-                        echo '<li><a href="">Login</a></li>';
+                        echo '<li><a href="Game.php?Game=' . $row[GameID] . '">Game with id '.$row[GameID]   ' </a></li>';
                         $row = mysqli_fetch_row($r);
-                    }
-                    //:V   
+                    }    <?php 
+            if(isset($_GET['error'])){
+                if($_GET['error'] == 1){
+                    ?> 
+                    <p><class='error'> ERROR YA HAS PASADO LOS 5 JUEGOS</p>
                 }
             ?></p>
     </div>
