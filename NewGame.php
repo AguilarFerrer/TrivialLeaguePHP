bf1ba399269a0984c33e19d357a2ee29a0f0494b
AguilarFerrer <felixaguilarferrer@gmail.com>
2018-01-20 15:00:42 +0100

    Added comments
    -Restructured the code.

--- NewGame.php
+++ NewGame.php
@@ -1,24 +1,37 @@
 <?php
+    //Here we open the connection to the database and the function redirect.
     require ('./SQL/mysqli_connect.php');
     require ('./includes/redirect.php');
-    $ID = $_COOKIE['ID'];
-    $q = "SELECT Count(GameID) AS Count FROM Games WHERE UserID=$ID";
+    //Here the query is a select to obtein the number of games that the user have started.
+    $q = "SELECT Count(GameID) AS Count FROM Games WHERE UserID=". $_COOKIE['ID'];
+    //Executed the query.
     $r = @mysqli_query ($dbc, $q);
+    //Now it obtein the data from the operation done before.
     $count = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+    //If the number of games active is 5, this will be redirected to the menu with an error code.
     if ($count['Count'] == 5){
         mysqli_close($dbc);
         redirect('Menu.php?error=1');
     }
+    //If is less than 5, this will create a new game.
     else{
-        $q = "INSERT INTO Games (UserID) Values ($ID)";
+        // This query creates the new game inserting the UserID and creating the GameID.
+        $q = "INSERT INTO Games (UserID) Values (" . $_COOKIE['ID'] . ")";
+        //Executed the query.
         $r = @mysqli_query ($dbc, $q);
-        $q = "SELECT MAX(GameID) AS Max FROM Games WHERE UserID=$ID";
+        //Now we obtein the GameID Created now by the last query.
+        $q = "SELECT MAX(GameID) AS Max FROM Games WHERE UserID=" . $_COOKIE['ID'];
+        //Executed the query.
         $r = @mysqli_query ($dbc, $q);
+        //And created a variable with the GameID obteined.
         $GameID = mysqli_fetch_array ($r, MYSQLI_ASSOC);
-        $GameID = $GameID['Max'];
-        $q = "INSERT INTO Ranks (RankID, GameID, Correct, Points) Values ($ID, $GameID, 0, 0)";
+        //Then insert into ranks the information obteined before. UserID, GameID and setting the points and correct answers to 0.
+        $q = "INSERT INTO Ranks (RankID, GameID, Correct, Points) Values (" . $_COOKIE['ID'] . "," . $GameID['Max'] . ", 0, 0)";
+        //Executed the query.
         $r = @mysqli_query ($dbc, $q);
+        //Close the connection to the database.
         mysqli_close($dbc);
-        redirect('Game.php?Game=' . $GameID);
+        //Redirect the user to the Game page with the GameID with get method.
+        redirect('Game.php?Game=' . $GameID['Max']);
     }
 ?>
