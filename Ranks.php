a316f7fd4d5c316290725ad48b6093f2d36c71b7
badking95 <mdlrauldf@gmail.com>
2018-01-21 16:04:06 +0100

    modify tables

--- Ranks.php
+++ Ranks.php
@@ -1,34 +1,24 @@
 <style>
-h1 { text-align:center; }
-aside {
-    display:flex;
-    flex-flow:row wrap;
-    align-content: center;
-    align-items: center;
-    justify-content:center;
-    padding: 2%;
-    widyh:100%;
-}   
+h1 {
+    text-align:center;
+}
 table {
-    margin-right: 10%;
     text-align: center;    
 }  
 table, th, td {
     border: 1px solid black;
     border-collapse: collapse;
-}
-th, td { padding: 15px; }
+}    
 </style>
-
 <?php
     require("./SQL/mysqli_connect.php");
     include('./includes/headerlogin.html');
 ?>
-<aside>
+<div class="row">
 <?php
     $q = "SELECT Name, Correct, Points FROM Ranks AS R INNER JOIN Users AS S ON S.UserID = R.RankID ORDER BY Points DESC LIMIT 20";
     $r = @mysqli_query ($dbc, $q);
-    $txt= '<p id="content"><table><tr><th>Position</th><th>Name</th><th>Points</th><th>Correct Questions</th>';
+    $txt= '<table><tr><th>Position</th><th>Name</th><th>Points</th><th>Correct Questions</th>';
     $row= mysqli_fetch_row($r);
     $int=1;
     while(!empty($row)) {
@@ -37,17 +27,24 @@ th, td { padding: 15px; }
         <td>' . $row[0] . '</td>
         <td>' . $row[2] . '</td>
         <td>' . $row[1] . '</td>
-    </tr><br>';
+    </tr>';
         $row = mysqli_fetch_row($r);
         $int++;
     }
-    echo "<h1>Global Ranking</h1";
-    echo $txt . '</table></p>';
+    echo '
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Global Ranking</h1> 
+                
+        ';
+                
+    echo $txt . '</table></div>';
     
     $cookie = $_COOKIE['ID'];
     $u = "SELECT Name, Correct, Points FROM Ranks AS R INNER JOIN Users AS S ON S.UserID = R.RankID WHERE UserID=$cookie ORDER BY Points DESC LIMIT 20";
     $r = @mysqli_query ($dbc, $u);
-    $txt= '<p id="content"> <table><tr><th>Name</th><th>Points<th>Correct Questions</th>';
+    $txt= '<table><tr><th>Name</th><th>Points<th>Correct Questions</th>';
     $row= mysqli_fetch_row($r);
     $int=1;
     while(!empty($row)) {
@@ -55,16 +52,17 @@ th, td { padding: 15px; }
         <td>' . $row[0] . '</td>
         <td>' . $row[2] . '</td>
         <td>' . $row[1] . '</td>
-    </tr><br>';
+    </tr>
+    
+    ';
         $row = mysqli_fetch_row($r);
         $int++;
     }
-    echo "<h1>My Records</h1";
-    echo $txt . '</table></p>';
+    echo '<div class="col-md-6">
+    <h1>My Records</h1>';
+    echo $txt . '</table></div>
+    </div>
+        </div>';
         
 ?>
-</aside>
-
-<?php
-    include ('./includes/footer.html');
-?>
+</div>
