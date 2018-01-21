f46ec67049a64797125ea396256bd9e4942b1d49
AguilarFerrer <felixaguilarferrer@gmail.com>
2018-01-21 16:44:08 +0100

    Added comments

--- includes/redirect.php
+++ includes/redirect.php
@@ -1,8 +1,11 @@
 <?php
+//This function is used to redirect the user to another page.
 function redirect ($page = 'index.php') {
+	//Generates the url.
 	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
 	$url = rtrim($url, '/\\');
 	$url .= '/' . $page;
+	//Directs the user into the introduced url.
 	header("Location: $url");
 	exit();
 }
